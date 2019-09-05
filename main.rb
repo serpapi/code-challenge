463f9660ab04d3cf3f0b5ac82770639da24da850
Ilya Zub <zaoooza92@gmail.com>
2019-09-05 09:59:50 +0300

    correctly parse images

--- main.rb
+++ main.rb
@@ -1,27 +1,31 @@
 require 'bundler/setup'
 
-require 'open-uri'
 require 'nokogiri'
 
-Artwork = Struct.new(:name, :image, :link, :extensions, keyword_init: true)
+Artwork = Struct.new(:name, :link, :image, :extensions, keyword_init: true)
 
-class Parser
+class ArtworksParser
   def parse
     url = './files/van-gogh-paintings.html'
-    html = open(url)
+    html = File.open(url).read
 
     doc = Nokogiri::HTML(html)
 
-    artworks = doc.css('.klitem').map do |painting|
-      artwork = Artwork.new(
+    images = html.scan(/var s='(data:image\/jpeg;base64,\S+)';/).map { |image| image[0] }
+
+    artworks = doc.css('.klitem').each_with_index.map do |painting, index|
+      link = "https://www.google.com#{painting.attr('href')}"
+
+      extensions = painting.at_css('.klmeta') ? [ painting.at_css('.klmeta').text ] : nil
+      
+      image = images[index] || nil
+      
+      Artwork.new(
         name: painting.at_css('.kltat').content,
-        image: painting.at_css('img') ? painting.at_css('img').attr('src') : nil,
-        link: painting.attr('href'),
+        link: link,
+        image: image,
+        extensions: extensions
       )
-
-      artwork[:extensions] = [ painting.at_css('.klmeta').text ] if painting.at_css('.klmeta')
-
-      artwork
     end
 
     result = { artworks: artworks }
--- main_spec.rb
+++ main_spec.rb
@@ -1,8 +1,8 @@
-require './main.rb'
+require_relative './main.rb'
 
-RSpec.describe Parser do
+RSpec.describe ArtworksParser do
   before :all do
-    parser = Parser.new
+    parser = ArtworksParser.new
     parse_results = parser.parse
 
     @artworks = parse_results[:artworks]
@@ -14,12 +14,10 @@ RSpec.describe Parser do
     it { is_expected.to be_a(Array) }
     it { is_expected.to have(51).items }
 
-    describe 'Artworks' do
-      it { is_expected.to all( be_a(Artwork) ) }
-      it { is_expected.to all( have_attributes(name: be_a(String)) ) }
-      it { is_expected.to all( have_attributes(link: be_a(String)) ) }
-      it { is_expected.to all( have_attributes(image: be_a(String).or(be(nil)) ) ) }
-      it { is_expected.to all( have_attributes(extensions: be_a(Array).or(be(nil)) ) ) }  
-    end
+    it { is_expected.to all( be_a(Artwork) ) }
+    it { is_expected.to all( have_attributes(name: be_a(String)) ) }
+    it { is_expected.to all( have_attributes(link: be_a(String)) ) }
+    it { is_expected.to all( have_attributes(image: be_a(String).or(be(nil)) ) ) }
+    it { is_expected.to all( have_attributes(extensions: be_a(Array).or(be(nil)) ) ) }
   end
 end
