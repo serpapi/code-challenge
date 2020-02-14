d6e072f8432398c20ca29f5c5b6a0807b1d0786c
Hugo Borges - agaelebe <agaelebe@gmail.com>
2020-02-14 17:12:04 -0300

    extrct links

--- lib/artwork_extractor.rb
+++ lib/artwork_extractor.rb
@@ -5,6 +5,7 @@ class ArtworkExtractor
   attr_reader :document
 
   CARDS_CSS_SELECTOR = 'a.klitem'
+  BASE_URL = 'https://www.google.com'
 
   def initialize(file)
     @document = Nokogiri::HTML.parse(file)
@@ -17,6 +18,11 @@ class ArtworkExtractor
   def extract_names
     name_attribute = 'aria-label'
     cards.map {|item| item[name_attribute] }
+  end
+
+  def extract_links
+    link_attribute = 'href'
+    cards.map {|item| "#{BASE_URL}#{item[link_attribute]}" }
   end
 
   private
--- rspec/artwork_extractor_spec.rb
+++ rspec/artwork_extractor_spec.rb
@@ -18,8 +18,16 @@ describe ArtworkExtractor do
     expect(subject.extract_names.last).to eq('Portrait of Adeline Ravoux')
   end
 
-  it 'extract the extensions from each card' do
-    binding.pry
+  it 'extracts 51 artwork links from the cards' do
+    expect(subject.extract_links.size).to eq(51)
+  end
+
+  it 'extracts the first link from the card' do
+    expect(subject.extract_links.first).to eq('https://www.google.com/search?gl=us&hl=en&q=The+Starry+Night&stick=H4sIAAAAAAAAAONgFuLQz9U3MI_PNVLiBLFMzC3jC7WUspOt9Msyi0sTc-ITi0qQmJnFJVbl-UXZxY8YI7kFXv64JywVMGnNyWuMXlxEaBJS4WJzzSvJLKkUkuLikYLbrcEgxcUF5_EsYhUIyUhVCC5JLCqqVPDLTM8oAQDmNFnDqgAAAA&npsic=0&sa=X&ved=0ahUKEwiL2_Hon4_hAhXNZt4KHTOAACwQ-BYILw')
+  end
+
+  xit 'extract the extensions from each card' do
+    pending
   end
 
 end
