a64b1b6e1670741d25b64b96a2e8e719ae6c9ad5
Hugo Borges - agaelebe <agaelebe@gmail.com>
2020-02-14 18:54:54 -0300

    match first image

--- lib/artwork_extractor.rb
+++ lib/artwork_extractor.rb
@@ -25,10 +25,22 @@ class ArtworkExtractor
     cards.map {|item| "#{BASE_URL}#{item[link_attribute]}" }
   end
 
+  def extract_images
+    cards.map do |item|
+      extract_image_data(item.at_css('g-img img')['id'])
+    end
+  end
+
   private
 
   def cards
     @document.css(CARDS_CSS_SELECTOR)
+  end
+
+  def extract_image_data(image_id)
+    base64_regexp = /(data:image\/jpeg;base64,(?:[A-Za-z0-9+\/]{4}\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?)2Q\\x3d\\x3d';var ii=\[/
+    # return first match group
+    base64_regexp.match(html)[1]
   end
 
 end
