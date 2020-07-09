7934e8c528d63e7e4ec679498fd10b508267a98e
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2020-07-09 15:50:02 +0500

    Additional images scraped, tests changed accordingly

--- image_extractor/__init__.py
+++ image_extractor/__init__.py
@@ -2,28 +2,70 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 
+class Driver():
+
+    def __enter__(self):
+        self.driver = webdriver.Chrome()
+        return self.driver
+
+    def __exit__(self, *args, **kwargs):
+        self.driver.quit()
+
+
+
 
 def extract_images(filelame):
-    driver = webdriver.Chrome()
-    driver.get("file://%s" % filelame)
-    divs = driver.find_elements_by_class_name('klitem-tr')
-    result = [] 
-    for d in divs:
-        src = d.find_element_by_tag_name('img').get_attribute('src')
-        name = d.find_element_by_class_name('kltat').text
-        link = d.find_element_by_tag_name('a').get_attribute('href').replace('file://', 'https://www.google.com')
-        try:
-            extensions = d.find_element_by_class_name('klmeta').text
-        except NoSuchElementException:
-            extensions = None
-        item = {
-            "name": name,
-            "link": link,
-            "image": src
+    with Driver() as driver:
+        driver.get("file://%s" % filelame)
+        divs = driver.find_elements_by_class_name('klitem-tr')
+        carousel = [] 
+        top_stories = []
+        search_suggestions = []
+        for d in divs:
+            src = d.find_element_by_tag_name('img').get_attribute('src')
+            name = d.find_element_by_class_name('kltat').text
+            link = d.find_element_by_tag_name('a').get_attribute('href').replace('file://', 'https://www.google.com')
+            try:
+                extensions = d.find_element_by_class_name('klmeta').text
+            except NoSuchElementException:
+                extensions = None
+            item = {
+                "name": name,
+                "link": link,
+                "image": src
+            }
+            if extensions:
+                item['extensions'] = [extensions]
+            carousel.append(item)
+        top_stories_containers = driver.find_elements_by_class_name('So9e7d')
+        for c in top_stories_containers:
+            try:
+                img = c.find_element_by_tag_name('img')
+            except NoSuchElementException:
+                continue
+            src = img.get_attribute('src')
+            title = c.find_element_by_class_name('mRnBbe').text
+            link = c.find_element_by_tag_name('a').get_attribute('href')
+            top_stories.append({
+                "image": src,
+                "name": title,
+                "link": link
+            })
+        suggestion_link = driver.find_element_by_link_text('People also search for')
+        suggestion_containers = suggestion_link.find_elements_by_xpath('../../div[@class="AAXrR lfNb6b"]//div[@data-reltype="sideways"]')
+        for c in suggestion_containers:
+            src = c.find_element_by_tag_name('img').get_attribute('src')
+            title = c.find_element_by_class_name('ellip').get_attribute('innerText')
+            link = c.find_element_by_tag_name('a').get_attribute('href').replace('file://', 'https://www.google.com')  
+            search_suggestions.append({
+                "image": src,
+                "name": title,
+                "link": link
+            })          
+        return {
+            "carousel": carousel,
+            "top_stories": top_stories,
+            "search_suggestions": search_suggestions
         }
-        if extensions:
-            item['extensions'] = [extensions]
-        result.append(item)
-    return result
 
     
--- test/test_image_extractor.py
+++ test/test_image_extractor.py
@@ -13,9 +13,12 @@ class ImageExtractorTestCase(unittest.TestCase):
     def test_sample(self):
         html_filename = os.path.join(self.data_dir, 'van-gogh-paintings.html')
         result = extract_images(html_filename)
-        expected_array_filename = os.path.join(self.data_dir, 'expected-array.json')
-        with open(expected_array_filename, 'r') as f:
-            expected_array = json.loads(f.read())
-        self.assertEqual(result, expected_array)
+        carousel = result['carousel']
+        self.assertEqual(len(carousel), 51)
+        self.assertEqual(carousel[0]['name'], 'The Starry Night')
+        self.assertEqual(carousel[0]['link'], 'https://www.google.com/search?gl=us&hl=en&q=The+Starry+Night&stick=H4sIAAAAAAAAAONgFuLQz9U3MI_PNVLiBLFMzC3jC7WUspOt9Msyi0sTc-ITi0qQmJnFJVbl-UXZxY8YI7kFXv64JywVMGnNyWuMXlxEaBJS4WJzzSvJLKkUkuLikYLbrcEgxcUF5_EsYhUIyUhVCC5JLCqqVPDLTM8oAQDmNFnDqgAAAA&npsic=0&sa=X&ved=0ahUKEwiL2_Hon4_hAhXNZt4KHTOAACwQ-BYILw')
+        self.assertTrue('data:image/jpeg;base64' in carousel[0]['image'])
+        self.assertEqual(carousel[0]['extensions'], ['1889'])
+
             
 
