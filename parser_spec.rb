48b2eb875a732401d148f235a5f72577f0024214
Diego Viola <diego.viola@gmail.com>
2020-11-28 05:14:39 -0300

    Add more specs

--- parser_spec.rb
+++ parser_spec.rb
@@ -11,5 +11,21 @@ describe Parser do
     it "should return json output from the local file" do
       (JSON.parse(@parser.create)).include?('artworks')
     end
+
+    it "should contain 51 artworks" do
+      _(JSON.parse(@parser.create)['artworks'].size).must_equal 51
+    end
+
+    it "should create a extensions field when a year is present" do
+      w = JSON.parse(@parser.create)['artworks'][0]
+      _(w['name']).must_equal "The Starry Night"
+      _(w).include?('extensions')
+    end
+
+    it "should not create a extensions field when there's no year" do
+      w = JSON.parse(@parser.create)['artworks'][2]
+      _(w['name']).must_equal "Sunflowers"
+      _(w).wont_include('extensions')
+    end
   end
 end
