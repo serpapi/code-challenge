415949a3b78c15360f29df3b50b211a1f180eb41
Alaa Abdulridha <53356539+Alaa-abdulridha@users.noreply.github.com>
2021-01-14 00:50:10 +0200

    Update googleScraperTest.php

--- files/tests/googleScraperTest.php
+++ files/tests/googleScraperTest.php
@@ -3,61 +3,104 @@ use PHPUnit\Framework\TestCase;
 
 final class googleScraperTest extends TestCase {
 	
-	/** @test - Output HTML */
-public function getOutPutAsHTMLFile() {
-    $google = new googleScraper();
-    $google->_htmlFilesPath =  __DIR__."/files/";
-    $google->_saveOutPut =  __DIR__."/OutPut/";
-    $this->assertTrue($google->init()['status']);
-    
-    $google->_outPutType = "html";
-    foreach($google->_filesInPath as $file) {
-      echo "HTML Output Tests for Vangogh Paintings, filepath: ".$file."\n";
-      $returnData = $google->runCode($file);
-      if($returnData['status']) {
-        if(array_key_exists('artworks', $returnData['outputData'])){
-          echo "Has a `artworks`\n";
-          $attrExist = ['name', 'extensions', 'link', 'image'];
-          foreach($attrExist as $attr){
-            if(array_key_exists($attr, $returnData['outputData']['artworks'][0]))
-              echo "Has a ".$attr."\n";
-            else
-              echo "Not has a ".$attr."\n";
-          }
-        }
-        
-      }
-      
-      $this->assertTrue($google->runCode($file)['status']);
-    }
-  }
+	private $_attrValidate = [ 
+		[
+			'name'		=> 'name',
+			'validate'	=> 'is_string',
+			'type'		=> 'function'
+		],						
+		[
+			'name'		=> 'extensions',
+			'validate'	=> 'is_array',
+			'type'		=> 'function'
+		],						
+		[
+			'name'		=> 'link',
+			'validate'	=> '/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i',
+			'type'		=> 'regex'
+		],						
+		[
+			'name'		=> 'image',
+			'validate'	=> '/^data:image\/(?:gif|png|jpeg|bmp|webp)(?:;charset=utf-8)?;base64,(?:[A-Za-z0-9]|[+\/])+={0,2}/',
+			'type'		=> 'regex'
+		]
+	];
 	
-	/** @test - Output Json */
+	/** @test */
+	public function getOutPutAsHTMLFile() {
+		$google = new googleScraper();
+		$google->_htmlFilesPath =  __DIR__."/files/";
+		$google->_saveOutPut =  __DIR__."/OutPut/";
+		$this->assertTrue($google->init()['status']);
+		
+		$google->_outPutType = "html";
+		foreach($google->_filesInPath as $file) {
+			echo "Knowledge Graph for Claude Monet Paintings, filepath: ".$file."\n";
+			$returnData = $google->runCode($file);
+			if($returnData['status']) {
+				if(array_key_exists('artworks', $returnData['outputData'])){
+					echo "Has a `artworks`\n";
+					
+					foreach($this->_attrValidate as $attr){
+						if(array_key_exists($attr['name'], $returnData['outputData']['artworks'][0])){
+							$vaild = false;
+							if($attr['type'] == 'regex') {
+								$vaild = preg_match($attr['validate'], $returnData['outputData']['artworks'][0][$attr['name']]);
+							} elseif($attr['type'] == 'function') {
+								$vaild = call_user_func_array($attr['validate'], [$returnData['outputData']['artworks'][0][$attr['name']]]);
+							}
+							if($vaild)
+								echo "Has a ".$attr['name']."\n";
+							else
+								echo "Not has a ".$attr['name']."\n";
+						} else {
+							echo "Not has a ".$attr['name']."\n";
+						}
+					}
+				}
+				
+			}
+			
+			$this->assertTrue($google->runCode($file)['status']);
+		}
+	}
+	
+	/** @test */
 	public function getOutPutAsJSONFile() {
-    $google = new googleScraper();
-    $google->_htmlFilesPath =  __DIR__."/files/";
-    $google->_saveOutPut =  __DIR__."/OutPut/";
-    $this->assertTrue($google->init()['status']);
-    
-    $google->_outPutType = "json";
-    foreach($google->_filesInPath as $file) {
-      echo "Json Output Tests for Vangogh Paintings, filepath: ".$file."\n";
-      $returnData = $google->runCode($file);
-      if($returnData['status']) {
-        if(array_key_exists('artworks', $returnData['outputData'])){
-          echo "Has a `artworks`\n";
-          $attrExist = ['name', 'extensions', 'link', 'image'];
-          foreach($attrExist as $attr){
-            if(array_key_exists($attr, $returnData['outputData']['artworks'][0]))
-              echo "Has a ".$attr."\n";
-            else
-              echo "Not has a ".$attr."\n";
-          }
-        }
-        
-      }
-      
-      $this->assertTrue($google->runCode($file)['status']);
-    }
-  }
+		$google = new googleScraper();
+		$google->_htmlFilesPath =  __DIR__."/files/";
+		$google->_saveOutPut =  __DIR__."/OutPut/";
+		$this->assertTrue($google->init()['status']);
+		
+		$google->_outPutType = "json";
+		foreach($google->_filesInPath as $file) {
+			echo "Knowledge Graph for Claude Monet Paintings, filepath: ".$file."\n";
+			$returnData = $google->runCode($file);
+			if($returnData['status']) {
+				if(array_key_exists('artworks', $returnData['outputData'])){
+					echo "Has a `artworks`\n";
+					
+					foreach($this->_attrValidate as $attr){
+						if(array_key_exists($attr['name'], $returnData['outputData']['artworks'][0])){
+							$vaild = false;
+							if($attr['type'] == 'regex') {
+								$vaild = preg_match($attr['validate'], $returnData['outputData']['artworks'][0][$attr['name']]);
+							} elseif($attr['type'] == 'function') {
+								$vaild = call_user_func_array($attr['validate'], [$returnData['outputData']['artworks'][0][$attr['name']]]);
+							}
+							if($vaild)
+								echo "Has a ".$attr['name']."\n";
+							else
+								echo "Not has a ".$attr['name']."\n";
+						} else {
+							echo "Not has a ".$attr['name']."\n";
+						}
+					}
+				}
+				
+			}
+			
+			$this->assertTrue($google->runCode($file)['status']);
+		}
+	}
 }
