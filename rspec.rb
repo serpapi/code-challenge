c7bfa93eedc95d744c12beb4f888233b86cc093c
k <kagermanovtalks@gmail.com>
2021-04-24 01:32:10 -0700

    Last minute mistakes and ruby style, also some optimization in test

--- rspec.rb
+++ rspec.rb
@@ -1,38 +1,39 @@
 load "van-gogh-class.rb"
 require 'rspec/autorun'
 
-testing_batch={"batch"=>[{"query"=>"Van Gogh paintings","path"=>'files/van-gogh-paintings.html'},
+testing_batch={"batch"=>[
 {"query"=>"Da Vinci Paintings","path"=>'files/da-vinci-paintings.html'},
 {"query"=>"Books of 2021","path"=>'files/books-of-2021.html'},
-{"query"=>"Sinatra albums","path"=>'files/sinatra-albums.html'}]}
+{"query"=>"Sinatra albums","path"=>'files/sinatra-albums.html'},
+{"query"=>"Van Gogh paintings","path"=>'files/van-gogh-paintings.html'}
+]}
 testing_batch['batch'].each do |result|
-    link=result['path']
-    query=result['query']
-    describe "Checking query \'#{query}\' with filepath \'#{link}'\ ..." do
-        artworks=Scrape.execute(link)
-        if link == "code-challenge/files/van-gogh-paintings.html"
-            it "checks if the solution files are same" do
-                artworks=Scrape.execute(link)
-                expect(File.read("solution.json")).to eq(File.read("code-challenge/files/expected-array.json"))
-            end
-        end
-        artworks['artworks'].each do |artwork|
-            describe "Checking for #{artwork['name']}" do
-                it "Checks for name" do
-                    expect(artwork['name'].is_a? String)
-                end
-                it "Checks for link" do
-                    expect(artwork['link'].is_a? String)
-                end
-                it "Checks for image base64" do
-                    expect(artwork['link'].is_a? String)
-                end
-                unless artwork['extensions'].nil?
-                    it "Checks for extensions" do
-                        expect(artwork['extensions'].is_a? Array)
-                    end
-                end
-            end
-        end
+  link=result['path']
+  query=result['query']
+  describe "Checking query \'#{query}\' with filepath \'#{link}'\ ..." do
+    artworks=Scrape.execute(link)
+    if link == "code-challenge/files/van-gogh-paintings.html"
+      it "checks if the solution files are same" do
+        expect(File.read("solution.json")).to eq(File.read("files/expected-array.json"))
+      end
     end
+    artworks['artworks'].each do |artwork|
+      describe "Checking for #{artwork['name']}" do
+        it "Checks for name" do
+          expect(artwork['name'].is_a? String)
+        end
+        it "Checks for link" do
+          expect(artwork['link'].is_a? String)
+        end
+        it "Checks for image base64" do
+          expect(artwork['link'].is_a? String)
+        end
+        unless artwork['extensions'].nil?
+          it "Checks for extensions" do
+            expect(artwork['extensions'].is_a? Array)
+          end
+        end
+      end
+    end
+  end
 end
--- van-gogh-class.rb
+++ van-gogh-class.rb
@@ -1,29 +1,29 @@
 require 'nokogiri'
 require 'json'
 class Scrape
-    def self.execute(path)
-        file = File.read(path)
-        doc = Nokogiri::HTML(file)
-        carousel = doc.search("g-scrolling-carousel").first
-        anchors=[]
-        artworks=[]
-        carousel.search('a').each do |anchor|
-            if anchor.text.gsub("#{anchor.attribute('aria-label').value}","")==""
-                artwork={"name"=>anchor.attribute('aria-label').value,
-                "link"=>unless anchor.attribute('href').nil? then "https://www.google.com"+anchor.attribute('href').value end,
-                "image"=>begin if doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','').include? " " then nil else doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','') end rescue nil end}
-            else
-                artwork={"name"=>anchor.attribute('aria-label').value,
-                "extensions"=>[anchor.text.gsub("#{anchor.attribute('aria-label').value}","")],
-                "link"=>unless anchor.attribute('href').nil? then "https://www.google.com"+anchor.attribute('href').value end,
-                "image"=>begin if doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','').include? " " then nil else doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','') end rescue nil end}
-            end
-            artworks.append(artwork)
-        end
-        artworks={"artworks"=>artworks}
-        File.open("solution.json","w") do |f|
-            f.write(JSON.pretty_generate(artworks).lines[0..-2].join.gsub("\n  ","\n")[2..-2])
-        end
-        return artworks
+  def self.execute(path)
+    file = File.read(path)
+    doc = Nokogiri::HTML(file)
+    carousel = doc.search("g-scrolling-carousel").first
+    anchors=[]
+    artworks=[]
+    carousel.search('a').each do |anchor|
+      if anchor.text.gsub("#{anchor.attribute('aria-label').value}","")==""
+        artwork={"name"=>anchor.attribute('aria-label').value,
+        "link"=>unless anchor.attribute('href').nil? then "https://www.google.com"+anchor.attribute('href').value end,
+        "image"=>begin if doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','').include? " " then nil else doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','') end rescue nil end}
+      else
+        artwork={"name"=>anchor.attribute('aria-label').value,
+        "extensions"=>[anchor.text.gsub("#{anchor.attribute('aria-label').value}","")],
+        "link"=>unless anchor.attribute('href').nil? then ("https://www.google.com"+anchor.attribute('href').value).gsub("https://www.google.comhttps://www.google.com","https://www.google.com") end,
+        "image"=>begin if doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','').include? " " then nil else doc.text.split("#{anchor.search('img').first.attribute('id').value}")[0].gsub("\';var ii=[\'","").split("var s=\'")[-1].gsub('\\','') end rescue nil end}
+      end
+      artworks.append(artwork)
     end
+    artworks={"artworks"=>artworks}
+    File.open("solution.json","w") do |f|
+    f.write(JSON.pretty_generate(artworks).lines[0..-2].join.gsub("\n  ","\n")[2..-2])
+    end
+    return artworks
+  end
 end
