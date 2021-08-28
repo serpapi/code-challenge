a6db1b3fdda2031ac9060d8cc49042ba88f9aec6
juan garcia <juan.dw.ft@gmail.com>
2021-08-28 14:09:42 -0500

    Configure rspec and application folder structure

---
+++ lib/google/carousel.rb
@@ -0,0 +1,8 @@
+module Google
+  class Carousel
+    def initialize; end
+
+    def process
+    end
+  end
+end
---
+++ spec/google/carousel_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require_relative "../../lib/google/carousel"
+
+describe Google::Carousel do
+  it 'Nokogiri should return google html' do
+  end
+end
