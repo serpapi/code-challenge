97c55ca87e70480b290868d8829651d9c9e5ed6a
Gildas <gildas.lormeau@gmail.com>
2022-03-14 22:21:39 +0100

    move code into index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { getTextFromReader, parseDocument } from "./lib/deno-util.js";
 import { getCarouselData } from "./lib/g-scrolling-carousel.js";
+import { patchHTML } from "./lib/page-util.js";
 
 const JSON_INDENTING_SPACES = 2;
 
-const html = await getTextFromReader(Deno.stdin);
+let html = await getTextFromReader(Deno.stdin);
+html = patchHTML(html);
 const document = await parseDocument(html);
 const carouselData = getCarouselData(document);
 const json = JSON.stringify(carouselData, null, JSON_INDENTING_SPACES);
