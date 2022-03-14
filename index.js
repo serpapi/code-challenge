bd352966c0ed048f3c556b170c58501f599401a1
Gildas <gildas.lormeau@gmail.com>
2022-03-15 00:58:26 +0100

    rename page-util to html-util

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { getTextFromReader, parseDocument } from "./lib/deno-util.js";
-import { patchHTML } from "./lib/page-util.js";
+import { patch } from "./lib/html-util.js";
 import { getCarouselData } from "./lib/g-scrolling-carousel.js";
 
 const JSON_INDENTING_SPACES = 2;
 
 const html = await getTextFromReader(Deno.stdin);
-const patchedHtml = patchHTML(html);
-const document = parseDocument(patchedHtml);
+const htmlPatched = patch(html);
+const document = parseDocument(htmlPatched);
 const carouselData = getCarouselData(document);
 const json = JSON.stringify(carouselData, null, JSON_INDENTING_SPACES);
 console.log(json);
