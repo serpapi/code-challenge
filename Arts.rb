0ea272468e6ce03b87fd3ffa223f8efb264c4146
Mahesh Gudladona <mahesh.g@live.com>
2022-04-23 17:37:51 +0530

    Use css selector instead of xpath

--- Arts.rb
+++ Arts.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'Utils'
 
 module Arts
   class Artworks
@@ -21,22 +22,20 @@ private
     def get_artworks(doc)
       artworks=[]
       imageDictionary = get_image_dictionary(doc)
-      
-      elements = doc.xpath('/html/body/div[6]/div[3]/div[7]/div[1]/div/div/div[2]/div[2]/div/div/g-scrolling-carousel/div/div/div')
+
+      elements = doc.css('g-scrolling-carousel > div > div > div > a')
       elements.each do |l|
-        nameFromHtml = l.children[0].attributes['aria-label'].value
-        name= Utils.get_encoded_value(nameFromHtml)
-        imgTag= l.children.children.children.children.children[0].attributes["id"]
 
-        image = imageDictionary[imgTag.value] if !imgTag.nil?
+        imgTag= l.css('g-img img').first
 
-        extensions = l.children.children.children.children
-          .select{|x| !x.text.empty?}
-          .select{|x| !name.include?(Utils.get_encoded_value(x.text))}
-          .map{|x| x.text}
+        image = imageDictionary[imgTag.attributes["id"].value] if !imgTag.nil?
+        name = Utils.get_encoded_value(l.css("div div span").text)
+        extension = Utils.get_encoded_value(l.css("div div").text).sub(name,'')
+
+        extensions = if extension.nil? || extension.empty? then [] else [extension] end
         
-        href= l.children[0].attributes['href'].value
-        link = "https://www.google.com#{href}"
+        href= l.attributes['href'].value
+        link = if href.start_with? then href else "https://www.google.com#{href}" end
 
         artwork = Arts::Artwork.new(name,link,image,extensions)
         artworks.push(artwork)
@@ -45,14 +44,14 @@ private
     end
 
     def get_image_dictionary(doc)
-      images = doc.xpath("/html/body/div[6]/div[3]/script[4]").text.split("function()")
+      images = doc.css("script").select{|x| x.text.include?("data:image/jpeg;base64")}
+      imgs= images.map{|x| x.text}.flat_map{|x| x.scan(/\'\S+\'/)}
 
       imgDict = {}
-      images.each do |i|
-        vals = i.split("\'")
-        if(!vals[1].nil?)
-          imgDict[vals[3]] = vals[1].gsub "\\", ""
-        end
+      t =0
+      while (t+1)<imgs.count
+        imgDict[imgs[t+1].tr("'","")] = imgs[t].tr("'","").tr("\\","")
+        t=t+2
       end
       return imgDict
     end
--- main.rb
+++ main.rb
@@ -3,8 +3,11 @@ $LOAD_PATH << '.'
 require 'Arts'
 require 'Utils'
 
-destination_path = './files/actual.json'
 source_html_path = './files/van-gogh-paintings.html'
+destination_path = './files/actual.json'
+# source_html_path = './files/da-vinci-paintings.html'
+# destination_path = './files/actual-da-vinci.json'
+
 
 artworks = Arts::Artworks.new(source_html_path)
 
