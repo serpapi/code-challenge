628d68146b89012897989f7e62d42fb7a286ab56
Sebastian Quek <sebq13@gmail.com>
2022-08-12 10:24:50 +0800

    feat: scrape google carousel

---
+++ src/scrape-google-carousel.ts
@@ -0,0 +1,113 @@
+import * as cheerio from "cheerio";
+
+export type GoogleCarouselItem = {
+  name: string;
+  extensions: string[];
+  link: string;
+  image: string | null;
+};
+
+export class GoogleCarouselScraper {
+  public $: cheerio.CheerioAPI;
+
+  constructor(public html: string) {
+    this.html = html;
+    this.$ = cheerio.load(html);
+  }
+
+  extractItemsFromHTML(): GoogleCarouselItem[] {
+    // `a.klitem-tr` exists for newer carousels
+    // `a.klitem` exists for the example van gogh html
+    const items = this.$("a.klitem-tr, a.klitem")
+      .map((_, element) => {
+        const name = this.getName(element);
+        return {
+          name,
+          extensions: this.getExtensions(element, name),
+          link: this.getLink(element),
+          image: this.getImage(element),
+        };
+      })
+      .get();
+    return items;
+  }
+
+  private getName(element: cheerio.Element): string {
+    return element.attribs["aria-label"];
+  }
+
+  /**
+   * Based on the title attribute of the element, extracts out the extensions
+   * by getting the text that trails the name of the current item.
+   *
+   * Preconditions:
+   * - Trailing text is surrounded by one character, e.g. brackets: '(' & ')'
+   *
+   * E.g. `title`: "The Starry Night (1889)", `name`: "The Starry Night" -> 1889
+   *
+   * @param element
+   * @param name
+   * @returns array of extensions
+   */
+  private getExtensions(element: cheerio.Element, name: string): string[] {
+    const titleAttr = element.attribs.title;
+    const titleHasName = titleAttr.indexOf(name) !== -1;
+    if (titleHasName) {
+      const metaWithBrackets = titleAttr.slice(name.length).trim();
+      const meta = metaWithBrackets.slice(1, -1);
+      return [meta];
+    }
+    return [];
+  }
+
+  private getLink(element: cheerio.Element): string {
+    const prefix = "https://www.google.com";
+    const href = element.attribs.href;
+    if (href.startsWith(prefix)) {
+      // In the odd case that the href already contains the prefix,
+      // don't incorrectly append the prefix.
+      return href;
+    } else {
+      return `${prefix}${href}`;
+    }
+  }
+
+  /**
+   * Google relies on JavaScript to replace image sources with the actual
+   * images after the page loads. i.e. the initial image source in the HTML
+   * is inaccurate.
+   *
+   * This method searches the JavaScript to extract out the corresponding
+   * image URIs.
+   *
+   * Preconditions:
+   * - JavaScript is defined after the definition of the carousel
+   * - Within JavaScript, image URI (i.e. data:image...) is defined before the image ID
+   * - base64 image sources
+   *
+   * @param element
+   * @returns
+   */
+  private getImage(element: cheerio.Element): string | null {
+    const imageId = this.$(element).find("img").attr("id");
+    if (!imageId) {
+      return null;
+    }
+
+    // Corresponds to the definition found in the JavaScript code
+    const idxOfImageId = this.html.lastIndexOf(`'${imageId}'`);
+    if (idxOfImageId === -1) {
+      // Implies there's no corresponding image id in the JavaScript code, and so no image
+      return null;
+    }
+
+    // The target image URI can be found before the image id is defined
+    const targetSearchHtml = this.html.slice(0, idxOfImageId);
+    const imageUri = targetSearchHtml
+      .slice(targetSearchHtml.lastIndexOf("data:image")) // Narrow the search
+      .match(/(data:image.*?)'/)?.[1]; // Get the first capturing group
+
+    // TODO: figure out a better way to decode the URIs, i.e. `\x3d` -> `=`
+    return imageUri ? imageUri.replaceAll("\\", "") : null;
+  }
+}
