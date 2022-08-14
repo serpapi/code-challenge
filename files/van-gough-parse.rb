901c2ac8dc7e5092bb139c60fc3f17824a562a3e
Laxman-M-R <laxman.rm098@gmail.com>
2022-08-14 11:59:37 +0530

    added classes as per ruby way

--- files/van-gough-parse.rb
+++ files/van-gough-parse.rb
@@ -1,28 +1,45 @@
 require 'nokogiri'
 
-art_works = Hash.new
+class GoogleCarouselParser
+  def parse
+    html_file = "van-gogh-paintings.html"
 
-art_works["artworks"] = []
+    art_works = Hash.new
 
-html = File.open("van-gogh-paintings.html").read
+    art_works["artworks"] = []
 
-parsed_data = Nokogiri::HTML(html)
+    html_file = "van-gogh-paintings.html"
+
+    html = File.open(html_file).read
+
+    parsed_data = Nokogiri::HTML(html)
+
+    images_thumbnails = html.scan(/var s='(data:image\/jpeg;base64,\S+)';/).map { |image| image[0] }
+
+    parsed_data.css('.klitem').each_with_index.map do |art_work, index|
+      item_hash = Hash.new
+
+      item_hash["name"] = art_work.at_css('.kltat').content ? art_work.at_css('.kltat').content : "" 
+
+      item_hash["extensions"] = art_work.at_css('.klmeta') ? [art_work.at_css('.klmeta').text] : nil
+
+      item_hash["link"] = "https://www.google.com#{art_work.attr('href')}"
+
+      item_hash["image"] = images_thumbnails[index] || nil
+
+      art_works["artworks"].push(item_hash)
+    end
 
 
-images_thumbnails = html.scan(/var s='(data:image\/jpeg;base64,\S+)';/).map { |image| image[0] }
+   result = {'artworks': art_works}
+  end
+ 
+ end
 
-parsed_data.css('.klitem').each_with_index.map do |art_work, index|
-  item_hash = Hash.new
+ gp = GoogleCarouselParser.new
+ parse = gp.parse
+ 
 
-  item_hash["name"] = art_work.at_css('.kltat').content ? art_work.at_css('.kltat').content : "" 
 
-  item_hash["extensions"] = art_work.at_css('.klmeta') ? [art_work.at_css('.klmeta').text] : nil
 
-  item_hash["link"] = "https://www.google.com#{art_work.attr('href')}"
-
-  item_hash["image"] = images_thumbnails[index] || nil
-
-  art_works["artworks"].push(item_hash)
-end
-
-puts art_works
+puts parse
