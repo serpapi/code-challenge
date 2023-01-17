11a6d13a93eae30cc2b04ff05b97b5c494470598
Abundnce10 <michaelstitt@gmail.com>
2023-01-17 12:59:22 -0800

    Test: don't include the  attribute if carousel item doesn't have a year associated with it.

--- python_scraper/test_google_carousel_scraper.py
+++ python_scraper/test_google_carousel_scraper.py
@@ -24,9 +24,9 @@ class TestGoogleCarouselScraper(unittest.TestCase):
 
         # pass in carousel_item to parse_carousel_item()
         g.parse_carousel_item(carousel_item)
-        new_num_of_results = len(g.results)
 
         # results array should have one more item added
+        new_num_of_results = len(g.results)
         self.assertEqual(new_num_of_results, num_of_results + 1)
 
         # test values for last item in results array
@@ -35,7 +35,21 @@ class TestGoogleCarouselScraper(unittest.TestCase):
         self.assertEqual(new_result["extensions"], ["1889"])
         self.assertEqual(new_result["link"], "/search?Msyi0sTc")
         self.assertEqual(new_result["image"], "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
+
+        # new carousel item (this one doesn't contain a year)
+        carousel_item_2 = BeautifulSoup("""
+        <div class="MiPcId klitem-tr mlo-c r-isjKlGqu4DQg" jsl="$t t-K64WL0oiT1I;$x 0;" style="height:197px;width:120px;margin-right:8px"><a aria-label="Sunflowers" aria-posinset="3" aria-setsize="51" class="klitem" data-hveid="53" data-sp="2,44,44" data-ved="0ahUKEwiL2_Hon4_hAhXNZt4KHTOAACwQ-BYINQ" href="/search?0ahUKEwiL2_Hon4_hAhXNZt4KHTOAACwQ-BYINQ" role="button" style="height:193px;width:120px" title="Sunflowers"><div class="klzc" style="margin-bottom:0"><div class="klic" style="height:120px;width:120px"><g-img class="BA0A6c" style="height:120px;width:120px"><img alt="" class="rISBZc M4dUYb" data-deferred="1" data-key="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJa5Y-t9vzkSCGg06vPfh_mBSZj4JyJ3eE98cFeCkFxJCXcTI2qr1jbv3Bi0Pv5jxouU0" height="120" id="kximg2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="120"/></g-img></div></div><div><div class="kltat"><span>Sunflowers</span><wbr/></div></div></a></div>
+        """, "html.parser")
         
+        # pass in carousel_item_2 to parse_carousel_item()
+        g.parse_carousel_item(carousel_item_2)
+
+        # test values for last item (carousel_item_2) in results array
+        new_result = g.results[-1]
+        self.assertEqual(new_result["name"], "Sunflowers")
+        self.assertRaises(KeyError, lambda: new_result["extensions"])
+        self.assertEqual(new_result["link"], "/search?0ahUKEwiL2_Hon4_hAhXNZt4KHTOAACwQ-BYINQ")
+        self.assertEqual(new_result["image"], "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
 
 
 if __name__ == '__main__':
