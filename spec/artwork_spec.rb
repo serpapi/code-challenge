464f76c026f5b3b3d45abcc3049b4763207f2883
Queenie Peng <pengqueenie@gmail.com>
2023-02-21 15:25:46 -0500

    Add test for picasso. Remove unused json file.

--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -3,22 +3,13 @@ require 'json'
 describe 'Artworks JSON' do
   attr_reader :json, :expected
 
-  before :all do
-    @json = JSON.parse(File.read('lib/files/artworks.json'))
-    @expected = JSON.parse(File.read('lib/files/expected-array.json'))
-  end
-
-  it 'equals to expected JSON' do
-    expect(json).to eq(expected)
-  end
-
-  context 'when validating' do
+  context 'when validating van gogh' do
+    let(:json) { JSON.parse(File.read('lib/files/van-gogh.json')) }
     let(:artworks) { json['artworks'] }
 
     it 'artworks' do
       expect(artworks).to be_an(Array)
       expect(artworks).to all(be_a(Hash))
-      expect(artworks.length).to eq(expected['artworks'].length)
     end
 
     it 'name, extensions, link, image' do
@@ -32,9 +23,22 @@ describe 'Artworks JSON' do
     end
 
     it 'Café Terrace at Night' do
-      art = artworks[4]
+      art = artworks[5]
       expect(art['name']).to eq('Café Terrace at Night')
       expect(art['extensions']).to eq(['1888'])
+      expect(art['link']).to start_with('https://www.google.com')
+      expect(art['image']).to start_with('data:image/jpeg')
+    end
+  end
+
+  context 'when validating picasso' do
+    let(:json) { JSON.parse(File.read('lib/files/picasso.json')) }
+    let(:artworks) { json['artworks'] }
+
+    it 'Guernica' do
+      art = artworks.first
+      expect(art['name']).to eq('Guernica')
+      expect(art['extensions']).to eq(['1937'])
       expect(art['link']).to start_with('https://www.google.com')
       expect(art['image']).to start_with('data:image/jpeg')
     end
