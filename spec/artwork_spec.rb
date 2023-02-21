e8aab0dd03f8126890687ad450d057a4f9a6df51
Queenie Peng <pengqueenie@gmail.com>
2023-02-21 15:40:09 -0500

    Add mark twain test.

--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -3,7 +3,7 @@ require 'json'
 describe 'Artworks JSON' do
   attr_reader :json, :expected
 
-  context 'when validating van gogh' do
+  context 'when validating Van Gogh' do
     let(:json) { JSON.parse(File.read('lib/files/van-gogh.json')) }
     let(:artworks) { json['artworks'] }
 
@@ -31,7 +31,7 @@ describe 'Artworks JSON' do
     end
   end
 
-  context 'when validating picasso' do
+  context 'when validating Picasso' do
     let(:json) { JSON.parse(File.read('lib/files/picasso.json')) }
     let(:artworks) { json['artworks'] }
 
@@ -39,6 +39,19 @@ describe 'Artworks JSON' do
       art = artworks.first
       expect(art['name']).to eq('Guernica')
       expect(art['extensions']).to eq(['1937'])
+      expect(art['link']).to start_with('https://www.google.com')
+      expect(art['image']).to start_with('data:image/jpeg')
+    end
+  end
+
+  context 'when validating Mark Twain' do
+    let(:json) { JSON.parse(File.read('lib/files/mark-twain.json')) }
+    let(:artworks) { json['artworks'] }
+
+    it 'The Adventures of Tom Sawyer' do
+      art = artworks.first
+      expect(art['name']).to eq('The Adventures of Tom Sawyer')
+      expect(art['extensions']).to eq(['1876'])
       expect(art['link']).to start_with('https://www.google.com')
       expect(art['image']).to start_with('data:image/jpeg')
     end
