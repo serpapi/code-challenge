63f84be1bf8565215d1e386d60867ad3d226ffda
mariettasiuzdak <marietta.siuzdak@gmail.com>
2023-06-22 23:10:07 +0200

    Create marietta.py

---
+++ marietta.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup 
+import re
+import requests 
+
+site = requests.get("https://raw.githubusercontent.com/serpapi/code-challenge/master/files/van-gogh-paintings.html") #get webpage content
+test = requests.get(
+
+
+soup = BeautifulSoup(site.content, 'html.parser')
+images = soup.select('a[aria-posinset]')
+all_artworks =  []
+
+for i in images:
+    title = i.text.strip()
+    year = re.findall(r'\d+', title)
+    year = ''.join(year)
+    link = 'https://www.google.com' + i['href']
+    artwork = {}
+    artwork['title'] = title
+    artwork['year'] = year
+    artwork['link'] = link
+    all_artworks.append(artwork)
+
+print(all_artworks)
