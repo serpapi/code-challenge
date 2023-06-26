afc052d307f7e45f7c50e62ea0d48cb40c251228
Mat Banbury <matthewbanbury@gmail.com>
2023-06-25 21:01:32 -0400

    Replaces unnecessary 'data-key' specs and fixtures

--- spec/carousel_scraper/carousel_item_parser_spec.rb
+++ spec/carousel_scraper/carousel_item_parser_spec.rb
@@ -1,16 +1,16 @@
 require 'rspec'
 
 RSpec.describe CarouselScraper::CarouselItemParser do
+  subject(:parser) { described_class.new(node, encoded_images) }
+  let(:node) { Nokogiri::HTML(html).css('a').first }
   let(:encoded_images) do
     {
       "kximg0" => "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD..."
     }
   end
 
-  context 'when the image has a data-key' do
-    let(:html) { File.read('spec/fixtures/carousel_item_with_data_key.html') }
-    let(:node) { Nokogiri::HTML(html).css('a').first }
-    subject(:parser) { described_class.new(node, encoded_images) }
+  context 'with a valid carousel item' do
+    let(:html) { File.read('spec/fixtures/valid_carousel_item.html') }
 
     it 'parses the title' do
       expect(parser.title).to eq('The Starry Night')
@@ -33,29 +33,19 @@ RSpec.describe CarouselScraper::CarouselItemParser do
     end
   end
 
-  context 'when the image does not have a data-key' do
-    let(:html) { File.read('spec/fixtures/carousel_item_without_data_key.html') }
-    let(:node) { Nokogiri::HTML(html).css('a').first }
-    subject(:parser) { described_class.new(node, encoded_images) }
+  context 'with an invalid carousel item' do
+    let(:html) { File.read('spec/fixtures/invalid_carousel_item.html') }
 
-    it 'parses the title' do
-      expect(parser.title).to eq('The Yellow House')
+    describe '#valid_carousel_item?' do
+      it 'returns false' do
+        expect(parser.valid_carousel_item?).to be(false)
+      end
     end
 
-    it 'does not have an image URL' do
-      expect(parser.image).to be_nil
-    end
-
-    it 'returns the correct url' do
-      expect(parser.url).to eq('https://www.google.com/url-for-the-yellow-house')
-    end
-
-    it 'returns the correct year' do
-      expect(parser.year).to eq(['1888'])
-    end
-
-    it 'identifies a valid carousel item' do
-      expect(parser.valid_carousel_item?).to be true
+    describe '#transform' do
+      it 'returns nil' do
+        expect(parser.transform).to be_nil
+      end
     end
   end
 end
