20bee433524ae9ba98a65997d0454d12217c458a
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2023-07-14 23:35:44 +0300

    add other html documents and a main entrypoint script

--- main.rb
+++ main.rb
@@ -1,8 +1,25 @@
-require_relative "./challenge/challenge.rb"
+# frozen_string_literal: true
 
-fname = 'files/van-gogh-paintings.html'
+require 'json'
+require_relative './challenge/challenge'
 
-document = Challenge::HtmlFileParser.new(fname).parse
-result = Challenge::Parser.new(document).parse
+filenames = %w[
+  van-gogh-paintings.html
+  game_of_thrones_cast.html
+  christopher_tokien_books.html
+  george_w_bush_google_search.html
+]
 
-pp result
+filenames.each do |fname|
+  in_path = File.expand_path("files/#{fname}")
+
+  output_file_name = fname.sub('.html', '.json')
+  out_path = File.expand_path("output/#{output_file_name}")
+
+  puts "\nParsing file #{in_path}"
+  document = Challenge::HtmlFileParser.new(in_path).parse
+  result = Challenge::Parser.new(document).parse
+
+  puts "writing results to #{out_path}"
+  File.write(out_path, JSON.dump(result))
+end
