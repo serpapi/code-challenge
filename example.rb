75928e614802ebf985797bd135ba784d813dfd49
chukhraiartur <chukhraiartur@gmail.com>
2023-07-25 23:16:30 +0300

    update(example): added extraction of several HTML pages

--- example.rb
+++ example.rb
@@ -2,6 +2,13 @@ require_relative 'lib/google_artworks_parser'
 require 'json'
 
 parser = GoogleArtworksParser.new('./files/van-gogh-paintings.html')
-artworks = parser.artworks
+artworks = parser.get_results
+File.write('./outputs/van-gogh-paintings.json', JSON.pretty_generate(artworks))
 
-puts JSON.pretty_generate(artworks)
+parser.path = './files/van-gogh-paintings-new.html'
+artworks = parser.get_results
+File.write('./outputs/van-gogh-paintings-new.json', JSON.pretty_generate(artworks))
+
+parser.path = './files/picasso-paintings.html'
+artworks = parser.get_results
+File.write('./outputs/picasso-paintings.json', JSON.pretty_generate(artworks))
