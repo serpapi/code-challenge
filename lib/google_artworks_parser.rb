8a0e024a2532d45a147c3a254fa26b94ca22f5fd
chukhraiartur <chukhraiartur@gmail.com>
2023-07-26 00:39:35 +0300

    update(parser): added documentation comments

--- lib/google_artworks_parser.rb
+++ lib/google_artworks_parser.rb
@@ -1,14 +1,23 @@
 require 'nokolexbor'
 
 class GoogleArtworksParser
+  # Creates a new instance of GoogleArtworksParser with the specified path to the HTML file.
+  #
+  # @param path [String] The path to the HTML file containing Google Artworks data.
   def initialize(path)
     @path = path
   end
 
+  # Gets the current path to the HTML file.
+  #
+  # @return [String] The current path to the HTML file.
   def path
     @path
   end
 
+  # Sets a new path to the HTML file and clears memorized values if the new path is different from the current path.
+  #
+  # @param new_path [String] The new path to the HTML file.
   def path=(new_path)
     if @path != new_path
       @path = new_path
@@ -16,14 +25,23 @@ class GoogleArtworksParser
     end
   end
 
+  # Reads and retrieves the HTML content from the specified path.
+  #
+  # @return [String] The HTML content of the file.
   def get_html
     @html ||= File.open(@path).read
   end
 
+  # Extracts the images from the HTML content and caches them for subsequent calls.
+  #
+  # @return [Array<String>] An array of base64-encoded image strings.
   def get_images
     @images ||= extract_images
   end
 
+  # Retrieves the Google Artworks data.
+  #
+  # @return [Hash] A hash containing the artworks data or an error message if no artworks are found.
   def get_results
     artworks = []
 
@@ -40,6 +58,9 @@ class GoogleArtworksParser
 
   private
 
+  # Extracts and processes old version artworks data.
+  #
+  # @return [Array<Hash>] An array of hashes containing old version artworks data.
   def get_old_artworks
     old_artworks = []
 
@@ -58,6 +79,9 @@ class GoogleArtworksParser
     old_artworks
   end
 
+  # Extracts and processes new version artworks data.
+  #
+  # @return [Array<Hash>] An array of hashes containing new version artworks data.
   def get_new_artworks
     new_artworks = []
 
@@ -76,10 +100,16 @@ class GoogleArtworksParser
     new_artworks
   end
 
+  # Parses the HTML content using Nokolexbor and caches the result for subsequent calls.
+  #
+  # @return [Nokolexbor::HTML::Document] The parsed Nokolexbor document.
   def doc
     @doc ||= Nokolexbor::HTML(get_html)
   end
 
+  # Extracts and decodes the images from the HTML content.
+  #
+  # @return [Array<String>] An array of base64-encoded image strings.
   def extract_images
     image_ids = doc.css('.rISBZc, .taFZJe').map { |node| node['id'] }
     
@@ -90,6 +120,7 @@ class GoogleArtworksParser
     end
   end
 
+  # Clears the cached values for HTML content, images, and the parsed document.
   def clear_memorized_values
     @html = nil
     @images = nil
