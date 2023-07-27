9297b52a5a41fd537754325524e349c2f0903d3b
Ashik Salman <ashiksp@gmail.com>
2023-07-27 14:03:57 +0530

    Added google base url to constant namespace module.

---
+++ lib/google_carousel/constants/google.rb
@@ -0,0 +1,11 @@
+module GoogleCarousel
+  module Constants
+    module Google
+      class << self
+        def base_url
+          'https://www.google.com'
+        end
+      end
+    end
+  end
+end
