44542615b676be28bee2f6a743b8804459d6983b
Daniel Prause <dadalino@gmail.com>
2023-08-20 12:41:10 +0200

    refactor/test: add buildings of bonn as an expected array

--- spec/carousel_parser_spec.rb
+++ spec/carousel_parser_spec.rb
@@ -10,10 +10,10 @@ describe Services::CarouselParser do
   end
   describe :original_challenge do
     before :all do
-      @browser.goto("file://#{File.dirname(__FILE__)}/../files/van-gogh-paintings.html")
+      @browser.goto("file://#{Dir.pwd}/files/van-gogh-paintings.html")
       @parsed_carousel = Services::CarouselParser.call(@browser.html)
       expected_file = File.open('files/expected-array.json')
-      @expected = JSON.parse("{ #{expected_file.read} } ")['artworks']
+      @expected = JSON.parse(expected_file.read)['artworks']
     end
 
     %w[name link year].each do |expected_test_attribute|
@@ -25,18 +25,35 @@ describe Services::CarouselParser do
     end
   end
 
-  describe :other_document do
+  describe :queen_song_writers do
     before :all do
-      @browser.goto("file://#{File.dirname(__FILE__)}/../files/queen-song-writers.html")
+      @browser.goto("file://#{Dir.pwd}/files/queen-song-writers.html")
       @parsed_carousel = Services::CarouselParser.call(@browser.html)
       expected_file = File.open('files/expected-artists.json')
-      @expected = JSON.parse("{ #{expected_file.read} } ")['artists']
+      @expected = JSON.parse(expected_file.read)['artists']
     end
 
     %w[name link image].each do |expected_test_attribute|
       it "gets the correct #{expected_test_attribute}s" do
         expected = @expected.map { |artist| artist[expected_test_attribute] }
         actual = @parsed_carousel.map { |artist| artist[expected_test_attribute] }
+        expect(expected).to eq(actual)
+      end
+    end
+  end
+
+  describe :buildings_in_bonn do
+    before :all do
+      @browser.goto("file://#{Dir.pwd}/files/buildings-in-bonn.html")
+      @parsed_carousel = Services::CarouselParser.call(@browser.html)
+      expected_file = File.open('files/expected-buildings.json')
+      @expected = JSON.parse(expected_file.read)['buildings']
+    end
+
+    %w[name link image].each do |expected_test_attribute|
+      it "gets the correct #{expected_test_attribute}s" do
+        expected = @expected.map { |building| building[expected_test_attribute] }
+        actual = @parsed_carousel.map { |building| building[expected_test_attribute] }
         expect(expected).to eq(actual)
       end
     end
