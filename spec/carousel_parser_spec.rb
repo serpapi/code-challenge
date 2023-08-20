61fac7170051e23b793a34a67d3dfeb7552c3f24
Daniel Prause <dadalino@gmail.com>
2023-08-20 19:41:33 +0200

    refactor: loading files in test cases

--- spec/carousel_parser_spec.rb
+++ spec/carousel_parser_spec.rb
@@ -6,16 +6,13 @@ require 'json'
 describe Services::CarouselParser do
   describe :original_challenge do
     before :all do
-      file_content = File.read("#{Dir.pwd}/files/van-gogh-paintings.html")
-      @parsed_carousel = Services::CarouselParser.call(file_content, 'artworks')['artworks']
-      expected_file = File.open('files/expected-array.json')
-      @expected = JSON.parse(expected_file.read)['artworks']
+      @expected, @actual = load_data('van-gogh-paintings.html', 'expected-array.json', 'artworks')
     end
 
     %w[name link year].each do |expected_test_attribute|
       it "gets the correct #{expected_test_attribute}s" do
         expected = @expected.map { |artwork| artwork[expected_test_attribute] }
-        actual = @parsed_carousel.map { |artwork| artwork[expected_test_attribute] }
+        actual = @actual.map { |artwork| artwork[expected_test_attribute] }
         expect(expected).to eq(actual)
       end
     end
@@ -23,16 +20,13 @@ describe Services::CarouselParser do
 
   describe :queen_song_writers do
     before :all do
-      file_content = File.read("#{Dir.pwd}/files/queen-song-writers.html")
-      @parsed_carousel = Services::CarouselParser.call(file_content, 'artists')['artists']
-      expected_file = File.open('files/expected-artists.json')
-      @expected = JSON.parse(expected_file.read)['artists']
+      @expected, @actual = load_data('queen-song-writers.html', 'expected-artists.json', 'artists')
     end
 
     %w[name link image].each do |expected_test_attribute|
       it "gets the correct #{expected_test_attribute}s" do
         expected = @expected.map { |artist| artist[expected_test_attribute] }
-        actual = @parsed_carousel.map { |artist| artist[expected_test_attribute] }
+        actual = @actual.map { |artist| artist[expected_test_attribute] }
         expect(expected).to eq(actual)
       end
     end
@@ -40,18 +34,24 @@ describe Services::CarouselParser do
 
   describe :buildings_in_bonn do
     before :all do
-      file_content = File.read("#{Dir.pwd}/files/buildings-in-bonn.html")
-      @parsed_carousel = Services::CarouselParser.call(file_content, 'buildings')['buildings']
-      expected_file = File.open('files/expected-buildings.json')
-      @expected = JSON.parse(expected_file.read)['buildings']
+      @expected, @actual = load_data('buildings-in-bonn.html', 'expected-buildings.json', 'buildings')
     end
 
     %w[name link image].each do |expected_test_attribute|
       it "gets the correct #{expected_test_attribute}s" do
         expected = @expected.map { |building| building[expected_test_attribute] }
-        actual = @parsed_carousel.map { |building| building[expected_test_attribute] }
+        actual = @actual.map { |building| building[expected_test_attribute] }
         expect(expected).to eq(actual)
       end
     end
+  end
+
+  def load_data(html, json, root_node_name)
+    file_content = File.read("files/#{html}")
+    actual = Services::CarouselParser.call(file_content, root_node_name)[root_node_name]
+    expected_file = File.open("files/#{json}")
+    expected = JSON.parse(expected_file.read)[root_node_name]
+
+    [expected, actual]
   end
 end
