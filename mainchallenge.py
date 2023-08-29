f5f7f12119f51068f677d43bf6b2eb5a350a3d0f
Joseph Laux <jlaux@josephs-mbp-2.mynetworksettings.com>
2023-08-29 14:16:26 -0400

    outline with functions and brainstorm

--- mainchallenge.py
+++ mainchallenge.py
@@ -2,12 +2,13 @@ import requests
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 
-url = "van-gogh-paintings.html"
+url = "https://raw.githubusercontent.com/serpapi/code-challenge/master/files/van-gogh-paintings.html"
 
 def main():
     paintings = []
     parseObject = parseHtml(url)
-
+    pictures = parseObject.find_all()
+    # find everything with "title" in line 65 and then add to
 
     return paintings
 
@@ -17,6 +18,16 @@ def parseHtml(url):
     soup = BeautifulSoup(webpage_text, 'html.parser')
     return soup
 
+def findPaintingAttributes():
+    painting_details = {}
+    painting_details["name"] = "insert here" # title before "("
+    painting_details["extensions"] = "insert here" # title regex "(...)"
+    painting_details["link"] = "insert here" # href
+    painting_details["image"] = "insert here" # src
+
+
+
+    return painting_details
 
 
 
