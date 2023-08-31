53d370f6cd48287f52eb7d0e46f00644c655f67d
Joshua Berger <bergerjosh11@gmail.com>
2023-08-31 11:58:17 -0600

    Update html_parser.rb

--- files/html_parser.rb
+++ files/html_parser.rb
@@ -1,34 +1,49 @@
 require 'nokogiri'
 require 'json'
 
+# Get the input HTML file path from command line arguments
+input_file_path = ARGV[0]
+
+if input_file_path.nil? || !File.exist?(input_file_path)
+  puts "Please provide a valid input HTML file path."
+  exit
+end
+
 # Load the HTML file
-html_file = File.open('van-gogh-paintings.html')
+html_file = File.open(input_file_path)
 doc = Nokogiri::HTML(html_file)
 
 # Initialize an array to store the paintings
 paintings = []
 
-# Extract paintings from the HTML
-doc.css('div.yrF3V').each do |painting|
-  name = painting.css('div.BNeawe.vvjwJb.AP7Wnd').text
-  extensions = painting.css('span.BNeawe.s3v9rd.AP7Wnd').text.split(',').map(&:strip)
-  link = painting.css('a')[0]['href']
-  image_data = painting.css('img')[0]['src']
+# Define the mapping of fields to their extraction methods
+field_mapping = {
+  "name" => ->(element) { element.css('h2').text },
+  "extensions" => ->(element) { element.css('p.date').text.split(',').map(&:strip) },
+  "link" => ->(element) { element.css('a').first['href'] },
+  "image" => ->(element) { element.css('img').first['src'] }
+}
 
-  paintings << {
-      "name" => name,
-      "extensions" => extensions,
-      "link" => link,
-      "image" => image_data
-  }
+# Extract paintings from the HTML
+doc.css('div.painting').each do |element|
+  painting = {}
+
+  field_mapping.each do |field, extraction_method|
+    painting[field] = extraction_method.call(element)
+  end
+
+  paintings << painting
 end
 
 # Create a JSON object containing the paintings array
 output = { "artworks" => paintings }
 
+# Generate the output file name based on the input file name
+output_file_path = input_file_path.gsub('.html', '_output.json')
+
 # Save the JSON to a file
-File.open('output.json', 'w') do |file|
+File.open(output_file_path, 'w') do |file|
   file.write(JSON.pretty_generate(output))
 end
 
-puts "Output saved to output.json"
+puts "Output saved to #{output_file_path}"
