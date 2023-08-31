c36fa77130ed194c58c79450ee49ac588873ccc2
Joshua Berger <bergerjosh11@gmail.com>
2023-08-31 11:46:13 -0600

    updated RSpec test

--- files/html_parser_spec.rb
+++ files/html_parser_spec.rb
@@ -4,8 +4,21 @@ require 'json'
 RSpec.describe 'HTML Parser' do
   describe 'parsing' do
     it 'extracts painting information from HTML' do
-      html = File.read('sample.html')
-      allow(File).to receive(:open).and_return(html)
+      # Load the HTML file for testing
+      html_filename = 'van-gogh-paintings.html'
+      html_content = File.read(html_filename)
+
+      # Stub the Nokogiri::HTML method to return the test HTML content
+      allow(Nokogiri).to receive(:HTML).and_return(Nokogiri::HTML(html_content))
+
+      # Call the parse method and get the result
+      result = HTMLParser.parse_paintings(html_filename)
+
+      # Load the expected result from a JSON file
+      expected_result = JSON.parse(File.read('expected-array.json'))
+
+      # Compare the parsed result with the expected result
+      expect(result).to eq(expected_result)
     end
   end
 
