09dfad77649d9f89c95012e0f6549c850152933b
Anton Zaharia <zaharia_anton@yahoo.com>
2023-09-04 17:58:05 +0100

    chore: scraping for each anchor tag from the carousel

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,11 @@
 require_relative "./scrapers/carousel"
+require "nokogiri"
 
 class Scraper
   include Scrapers::Carousel
 
   def initialize(file_path)
     @file = File.read(file_path)
+    @document = Nokogiri::HTML(@file)
   end
 end
--- lib/scrapers/carousel.rb
+++ lib/scrapers/carousel.rb
@@ -1,14 +1,22 @@
 module Scrapers
   module Carousel
     def scrape_carousel
-      [
+      anchors.map do |node|
         {
           name: nil,
           extensions: nil,
           link: nil,
           image: nil
         }
-      ]
+      end
+    end
+
+    def carousel
+      @_carousel ||= @document.search("g-scrolling-carousel").first
+    end
+
+    def anchors
+      @_anchors ||= carousel.search("a")
     end
   end
 end
