fa3ccdcdd641f2d549094a1323083bae44fc1386
Anton Zaharia <zaharia_anton@yahoo.com>
2023-09-04 17:35:47 +0100

    test: reorganised tests checkings for each attribute

--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -9,11 +9,26 @@ describe Scraper do
 
     let(:results) { subject.scrape_carousel }
 
-    it "matches exactly with the expected results" do
+    it "matches names with expected results" do
       results.each_with_index do |result, i|
         expect(expected[i]["name"]).to eq(result[:name])
+      end
+    end
+
+    it "matches extensions with expected results" do
+      results.each_with_index do |result, i|
         expect(expected[i]["extensions"]).to eq(result[:extensions])
+      end
+    end
+
+    it "matches link with expected results" do
+      results.each_with_index do |result, i|
         expect(expected[i]["link"]).to eq(result[:link])
+      end
+    end
+
+    it "matches image with expected results" do
+      results.each_with_index do |result, i|
         expect(expected[i]["image"]).to eq(result[:image])
       end
     end
