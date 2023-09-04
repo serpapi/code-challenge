5d593438c08710c68c32adedf16bb97044257ea1
Anton Zaharia <zaharia_anton@yahoo.com>
2023-09-04 17:28:47 +0100

    layout: carousel scraper addon for scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,9 @@
+require_relative "./scrapers/carousel"
+
 class Scraper
+  include Scrapers::Carousel
+
+  def initialize(file_path)
+    @file = File.read(file_path)
+  end
 end
---
+++ lib/scrapers/carousel.rb
@@ -0,0 +1,14 @@
+module Scrapers
+  module Carousel
+    def scrape_carousel
+      [
+        {
+          name: nil,
+          extensions: nil,
+          link: nil,
+          image: nil
+        }
+      ]
+    end
+  end
+end
