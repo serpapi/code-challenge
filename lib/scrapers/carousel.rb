5ad08162a0f1f0f22ba9e5ea6f45adf365f0d17a
Anton Zaharia <zaharia_anton@yahoo.com>
2023-09-04 20:38:46 +0100

    core: added command file to run the scrper

--- lib/scrapers/carousel.rb
+++ lib/scrapers/carousel.rb
@@ -3,6 +3,8 @@ require_relative "./carousel/card"
 module Scrapers
   module Carousel
     def scrape_carousel
+      raise "No Carousel found in the HTML document." unless carousel
+
       anchors.map { |node| Card.new(node, @document).to_hash }
     end
 
