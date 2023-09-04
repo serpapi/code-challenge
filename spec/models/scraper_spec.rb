5e3038eb895b481ae46c70dd2083f08be2ba7818
Anton Zaharia <zaharia_anton@yahoo.com>
2023-09-04 17:29:12 +0100

    test: scraper test for expected results

--- spec/models/scraper_spec.rb
+++ spec/models/scraper_spec.rb
@@ -1,7 +1,21 @@
 require "spec_helper"
+require "scraper"
+require "json"
 
 describe Scraper do
-  it "tests tests" do
-    expect(true).to eq(true)
+  context "van gogh paintings" do
+    let(:subject) { Scraper.new("spec/fixtures/van-gogh-paintings.html") }
+    let(:expected) { JSON.load(File.open("spec/fixtures/expected-array.json"))["artworks"] }
+
+    let(:results) { subject.scrape_carousel }
+
+    it "matches exactly with the expected results" do
+      results.each_with_index do |result, i|
+        expect(expected[i]["name"]).to eq(result[:name])
+        expect(expected[i]["extensions"]).to eq(result[:extensions])
+        expect(expected[i]["link"]).to eq(result[:link])
+        expect(expected[i]["image"]).to eq(result[:image])
+      end
+    end
   end
 end
