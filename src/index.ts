347526da74bb35d80d03b08e2871fea690a9f5c3
Derek Liang <fr.derekliang@gmail.com>
2023-09-06 16:56:12 -0700

    add debugger conf, add file reader and html parser

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-(() => {
-  console.log("hello world");
+import { readFile } from "node:fs/promises";
+import * as cheerio from "cheerio";
+import path from "path";
+
+(async () => {
+  const htmlFile = await readFile(
+    path.resolve("files/van-gogh-paintings.html"),
+  );
+  const $ = cheerio.load(htmlFile);
+  const caroselTableRows = $(".klitem-tr").toArray();
 })();
