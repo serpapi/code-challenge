782985c8e0f378fbc6d5ad1bb854281a8ed896dc
Derek Liang <fr.derekliang@gmail.com>
2023-09-06 17:08:41 -0700

    initial implementation of parsing

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,48 @@ import path from "path";
     path.resolve("files/van-gogh-paintings.html"),
   );
   const $ = cheerio.load(htmlFile);
-  const caroselTableRows = $(".klitem-tr").toArray();
+  const caroselTableRows = $(".klitem-tr .klitem").toArray();
+
+  const extractedRows = caroselTableRows.map((row) => {
+    const { attributes } = row;
+
+    let title: string | undefined;
+    let link: string | undefined;
+    let name: string | undefined;
+    const extensions: string[] = [];
+
+    attributes.forEach((a) => {
+      switch (a.name) {
+        case "title":
+          title = a.value;
+          break;
+        case "href":
+          link = a.value;
+          break;
+        case "aria-label":
+          name = a.value;
+          break;
+        default:
+          break;
+      }
+    });
+
+    const hasExtensions = title !== name;
+    if (title && hasExtensions) {
+      const left = title.indexOf("(");
+      const right = title.indexOf(")");
+
+      const hasYearExtension = left > -1 && right > -1;
+      if (hasYearExtension) {
+        const year = title.substring(left + 1, right);
+        extensions.push(year);
+      }
+    }
+
+    return {
+      name,
+      link,
+      extensions,
+    };
+  });
 })();
