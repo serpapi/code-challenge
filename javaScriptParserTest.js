c1c466d5dbccf6a80f28492093079cbcdecaaeee
Juan Villegas <juancarlos.villegas@gmail.com>
2023-11-29 18:36:57 -0600

    test if JSON generated file exists

--- javaScriptParserTest.js
+++ javaScriptParserTest.js
@@ -5,8 +5,15 @@ const cheerio = require('cheerio');
 describe('File Tests', function () {
 
     // HTML file should exist
-    it('should check if the file exists', function () {
+    it('Verify HTMl file exists', function () {
         const filePath = 'files/van-gogh-paintings.html';
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        assert.ok(fileContent, 'File exists');
+    });
+
+    // json generated file should exist
+    it('Verify JSON generated file exists', function () {
+        const filePath = 'van-gogh-paintings.json';
         const fileContent = fs.readFileSync(filePath, 'utf8');
         assert.ok(fileContent, 'File exists');
     });
