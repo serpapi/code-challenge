81e866453319a49b6252a1188557c13a8d2bf7e4
Juan Villegas <juancarlos.villegas@gmail.com>
2023-11-29 18:40:29 -0600

    test number of generated results.

--- javaScriptParserTest.js
+++ javaScriptParserTest.js
@@ -11,11 +11,25 @@ describe('File Tests', function () {
         assert.ok(fileContent, 'File exists');
     });
 
-    // json generated file should exist
+    // JSON generated file should exist
     it('Verify JSON generated file exists', function () {
         const filePath = 'van-gogh-paintings.json';
         const fileContent = fs.readFileSync(filePath, 'utf8');
         assert.ok(fileContent, 'File exists');
+    });
+
+    it('Verify painting named "The Starry Night" exists', function () {
+        const filePath = 'van-gogh-paintings.json';
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const paintingNameExists = fileContent.includes('The Starry Night');
+        assert.ok(paintingNameExists, 'The Starry Night painting exists');
+    });
+
+    it('Verify number of results in JSON file', function () {
+        const filePath = 'van-gogh-paintings.json';
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const jsonData = JSON.parse(fileContent);
+        assert.strictEqual(jsonData.length, 51, 'There are 51 results in the JSON file');
     });
 
 
