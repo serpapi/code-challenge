d50e67dac1d2901ab971f30ef0ac350763512324
Juan Villegas <juancarlos.villegas@gmail.com>
2023-11-29 18:34:25 -0600

    test if html file exists

--- javaScriptParserTest.js
+++ javaScriptParserTest.js
@@ -4,4 +4,12 @@ const cheerio = require('cheerio');
 
 describe('File Tests', function () {
 
+    // HTML file should exist
+    it('should check if the file exists', function () {
+        const filePath = 'files/van-gogh-paintings.html';
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        assert.ok(fileContent, 'File exists');
+    });
+
+
 });
