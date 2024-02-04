15bf2d6233baf40e020d7c3b4c278806beb6309e
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-02-03 20:27:34 -0500

    add function to get item nodes from carrousel node

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,49 @@ export const getHtmlNodeFromFile = (fileName: string) => {
     return parse(str);
 };
 
-export const getCarrouselNodeFromHtmlNode = (node: HTMLElement | null) => {
+export const getFirstCarrouselNodeFromHtmlNode = (node: HTMLElement | null) => {
     if (node === null) return null;
     const rawTagName = node.rawTagName;
     const carouselTagName = 'g-scrolling-carousel';
     if (node.tagName?.toLowerCase() === carouselTagName) return node;
-    const childrenArray = Array.from(node.childNodes) as HTMLElement[];
-    for (let i = 0; i < childrenArray.length; i++) {
-        const childResult = getCarrouselNodeFromHtmlNode(childrenArray[i]) as (HTMLElement | null);
+    const children = Array.from(node.childNodes) as HTMLElement[];
+    for (let i = 0; i < children.length; i++) {
+        const childResult = getFirstCarrouselNodeFromHtmlNode(children[i]) as (HTMLElement | null);
         if (childResult?.tagName.toLowerCase() === carouselTagName) return childResult;
     }
     return null;
 };
 
-const carrouselElementVangogh = getCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/van-gogh-paintings.html'));
-const carrouselElementMonet = getCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/monet-paintings.html'));
-const carrouselElementLeone = getCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/leone-movies.html'));
+export const nodeGetTotalLeafCount = (node: HTMLElement) => {
+    const children = Array.from(node.childNodes) as HTMLElement[];
+    if (children.length === 0) return 1;
+    let result = 0;
+    for (let i = 0; i < children.length; i++) {
+
+    }
+    return result;
+};
+
+
+export const getNodeWithCarrouselItems = (node: HTMLElement) => {
+    const possibleItemsListNode = node.childNodes[0].childNodes[0];
+    if (possibleItemsListNode.childNodes.length > 1) return possibleItemsListNode as HTMLElement;
+    return possibleItemsListNode.childNodes[0] as HTMLElement;
+};
+
+export const getItemNodesFromFileWithCarrousel = (fileName: string) => {
+    const carrousel = getFirstCarrouselNodeFromHtmlNode(getHtmlNodeFromFile(fileName));
+    if (carrousel === null) return [];
+    const itemsParent = getNodeWithCarrouselItems(carrousel);
+    return itemsParent.childNodes.map(c => c as HTMLElement);
+};
+
+const carrouselElementVangogh = getFirstCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/van-gogh-paintings.html'));
+const carrouselElementMonet = getFirstCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/monet-paintings.html'));
+const carrouselElementLeone = getFirstCarrouselNodeFromHtmlNode(getHtmlNodeFromFile('./files/leone-movies.html'));
+
+const vangoughItems = getItemNodesFromFileWithCarrousel('./files/van-gogh-paintings.html');
+const monetItems = getItemNodesFromFileWithCarrousel('./files/monet-paintings.html');
+const leoneItems = getItemNodesFromFileWithCarrousel('./files/leone-movies.html');
+
 console.log(getHtmlNodeFromFile('./files/van-gogh-paintings.html'));
