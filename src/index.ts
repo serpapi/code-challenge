0a6c7ae0a431ba1ed4451a2af4ed052506c49136
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-02-03 21:28:11 -0500

    add function that gets artwork array

--- src/index.ts
+++ src/index.ts
@@ -32,14 +32,36 @@ export const getItemNodesFromFileWithCarrousel = (fileName: string) => {
     return itemsParent.childNodes.map(c => c as HTMLElement);
 };
 
+interface ArtworkResult {
+    name: string,
+    extensions: string[],
+    link: string,
+    image: string,
+}
+
 export const getArtworkFromNodes = (carrouselItems: HTMLElement[]) => {
     return carrouselItems.map(i => {
-        const name = (i.childNodes[0] as HTMLElement).attributes['aria-label'];
-        //const extensions = i.childNodes[]
+        const result: ArtworkResult = {
+            name: '',
+            extensions: [],
+            link: '',
+            image: '',
+        };
+        const anchor = i.querySelector('a');
+        if (anchor !== null) {
+            result.name = anchor.attributes['aria-label'];
+            result.extensions = anchor.querySelectorAll('.klmeta').map(e => e.innerText);
+            result.link = anchor.attributes['href'];
+            const image = anchor.querySelector('img')?.attributes['src'];
+            result.image = image === undefined ? '' : image;
+        }
+        return result;
     });
 };
 
 const vangoghItems = getItemNodesFromFileWithCarrousel('./files/van-gogh-paintings.html');
 const leoneItems = getItemNodesFromFileWithCarrousel('./files/leone-movies.html');
+
+const artworkForVanGogh = getArtworkFromNodes(vangoghItems);
 
 console.log(getHtmlNodeFromFile('./files/van-gogh-paintings.html'));
