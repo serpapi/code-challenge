d6a30db55de10665076bab9f826cb73af3711fcf
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-15 11:33:48 -0600

    Add writeFile Method to Save Scraped Data to JSON

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ const path = require('path');
 const fs = require('fs');
 
 //should create a relative path on your local machine to find the .html file
-const url = 'file:' + path.resolve(__dirname, '../files/van-gogh-paintings.html');
+const url1 = 'file:' + path.resolve(__dirname, '../files/van-gogh-paintings.html');
 
 
-const vanGoghScrape = async () => {
+const vanGoghScrape = async (url) => {
     //spins up a headless browser and navigates to specified url
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
    
+    
     //this puppeteer method uses a combination of Array.from() & .querySelectorAll()
     //to scrape through every selector passed in as the first argument(s)
     const scrapedData = await page.$$eval('.klbar .klitem', 
@@ -26,9 +27,15 @@ const vanGoghScrape = async () => {
         image: e.querySelector('img').src
     })));
 
-    console.log(scrapedData);
+
+    //save the scraped data to a .json file, with error handling
+    fs.writeFile('vanGoghData.json', 
+    JSON.stringify(scrapedData, null, scrapedData.length), (err) => {
+        if (err) throw new Error('There was an error writing the scraped data');
+        console.log('Scraped data successfully written!');
+    });
 
     await browser.close();
 }
 
-vanGoghScrape();
+vanGoghScrape(url1);
