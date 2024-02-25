1cf2cea1e83da02218fdc5281fbe7c2d079b6d7f
Chisato Murakami <kiwipom1031@gmail.com>
2024-02-25 15:23:11 -0800

    add keyword in result

--- files/scraper_tool.rb
+++ files/scraper_tool.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'nokogiri'
+require 'open-uri'
+
 # Class that scrapes carousel that returns json
 class ScraperTool
   def initialize(url)
@@ -9,7 +12,28 @@ class ScraperTool
   attr_reader :url
 
   def scrape_carousel
+    doc = Nokogiri::HTML(File.open(url))
+    first_carousel = doc.at('g-scrolling-carousel')
+    result_key = keyword(first_carousel)
+
     result_hash = {}
+    result_hash[result_key] = []
     result_hash.to_json
+  end
+
+  private
+
+  def keyword(carousel)
+    carousel_ancestor = find_carousel_ancestor(carousel)
+    carousel_heading = carousel_ancestor.at('[role="heading"]')
+    texts = carousel_heading.xpath('.//text()')
+
+    texts.last.text.downcase
+  end
+
+  def find_carousel_ancestor(carousel)
+    current_node = carousel
+    current_node = current_node.parent until current_node.at('[role="heading"]')
+    current_node
   end
 end
--- files/spec/scraper_tool_spec.rb
+++ files/spec/scraper_tool_spec.rb
@@ -9,7 +9,7 @@ describe ScraperTool do
     root_dir = File.expand_path('.')
     url = "#{root_dir}/files/expected-array.json"
     file = File.read(url)
-    @data_hash = JSON.parse(file)
+    @expected_result = JSON.parse(file)
   end
 
   it 'runs' do
@@ -18,21 +18,31 @@ describe ScraperTool do
 
   it 'initializes with a url' do
     root_dir = File.expand_path('.')
-    url = "#{root_dir}/files/van-gogh-paintings.html}"
+    url = "#{root_dir}/files/van-gogh-paintings.html"
 
     tool = ScraperTool.new(url)
 
     expect(tool.url).to eq(url)
   end
 
-  it 'returns json' do
+  it 'has a keyword' do
     root_dir = File.expand_path('.')
-    url = "#{root_dir}/files/van-gogh-paintings.html}"
+    url = "#{root_dir}/files/van-gogh-paintings.html"
 
     tool = ScraperTool.new(url)
+    result_hash = JSON.parse(tool.scrape_carousel)
 
-    expect(JSON.parse(tool.scrape_carousel)).to eq(@data_hash)
+    expect(result_hash.keys).to eq(@expected_result.keys)
   end
+
+  # it 'returns json' do
+  #   root_dir = File.expand_path('.')
+  #   url = "#{root_dir}/files/van-gogh-paintings.html"
+
+  #   tool = ScraperTool.new(url)
+
+  #   expect(JSON.parse(tool.scrape_carousel)).to eq(@expected_result)
+  # end
 
   # it 'returns empty json if file is not found' do
   # end
