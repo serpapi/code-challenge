21d76d8543e598a8df84192d6493797f67d26e7c
Bailey McKelway <bjlmckelway@gmail.com>
2024-03-27 16:46:38 -0400

    getting parent component of images

--- main.rb
+++ main.rb
@@ -10,8 +10,26 @@ html_content = File.open(html_file)
 
 doc = Nokogiri::HTML(html_content)
 
-title = doc.css('title').text
-puts "Title of the page: #{title}"
+images_data = []
+parent = doc.css('#_c2yRXMvVOs3N-QazgILgAg93')
 
+parent.css('a').each do |link|
+  title = link['title']
+  href = link['href']
+
+  image_src = link.css('img').first['src'] unless link.css('img').empty?
+  puts link.css('img')
+  extensions = title.scan(/\((.*?)\)/).flatten
+
+  name = title.gsub(/\s*\([^)]+\)/, '')
+  images_data << {
+    name: name,
+    extensions: extensions,
+    link: href,
+ 
+  }
+end
+
+# puts images_data.inspect
 
 html_content.close
