a96ac1db95ff607bf275a6c1e969207cb46e3c79
Rodrigo Marques <rodrigomarques1993@gmail.com>
2024-07-14 18:10:45 -0300

    Improve specs
    - Add `expected-array.json` to match the correct output

--- spec/lib/paintings_extractor_spec.rb
+++ spec/lib/paintings_extractor_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
+require 'json'
 
 RSpec.describe PaintingsExtractor do
   describe '#extract_paintings' do
     subject { described_class.new(html).extract_paintings }
 
     let(:html) { File.read('spec/fixtures/van-gogh-paintings.html') }
+    let(:expected_array) { JSON.parse(File.read('spec/fixtures/expected-array.json'))['artworks'] }
 
     it 'extracts paintings correctly' do
       is_expected.not_to be_empty
-      expect(subject.first).to include(:image, :link, :name, :extensions)
+
+      expect(subject).to eq(expected_array)
     end
   end
 end
