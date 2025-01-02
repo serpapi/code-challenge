e5bf4e4e50d178662c8d8dcb4d8c4f8aced10c4d
Mike Rose <michaelrose151@gmail.com>
2025-01-02 16:15:32 -0500

    Start writing tests comparing my results with expected results

---
+++ spec/parse_van_gogh_paintings_spec.rb
@@ -0,0 +1,60 @@
+require_relative '../parse_van_gogh_paintings'
+require 'json'
+
+# TODO: Test to match my file result with provided expected `.json` file result - overall match, and then also by each key as that would help with debugging?
+
+RSpec.describe 'parse_van_gogh_paintings' do
+  let(:expected_result) do
+    file_path = 'files/expected-array.json'
+    JSON.parse(File.read(file_path))["artworks"]
+  end
+
+  let(:generated_result) do
+    file_path = 'files/van-gogh-paintings.html'
+    parse_van_gogh_paintings(file_path)
+  end
+
+  context 'when comparing all artworks' do
+    # TODO: Fix this
+    # it 'matches the all the artworks' do
+    #   expect(generated_result).to eq(expected_result)
+    # end
+
+    it 'matches the total number of artworks' do
+      expect(generated_result.size).to eq(expected_result.size)
+    end
+  end
+
+  context 'when comparing individual artworks' do
+    it 'matches the name for each artwork' do
+      generated_result.each_with_index do |artwork, index|
+        expect(artwork["name"]).to eq(expected_result[index]["name"])
+      end
+    end
+
+    # TODO: Fix this
+    # it 'matches the extensions for each artwork' do
+    #   generated_result.each_with_index do |artwork, index|
+    #     expect(artwork["extensions"]).to eq(expected_result[index]["extensions"])
+
+    #     ap artwork["extensions"]
+    #     puts "vs"
+    #     ap expected_result[index]["extensions"]
+    #     puts "-------"
+    #   end
+    # end
+
+    it 'matches the link for each artwork' do
+      generated_result.each_with_index do |artwork, index|
+        expect(artwork["link"]).to eq(expected_result[index]["link"])
+      end
+    end
+
+    # TODO: Fix this
+    # it 'matches the image for each artwork' do
+    #   generated_result.each_with_index do |artwork, index|
+    #     expect(artwork["image"]).to eq(expected_result[index]["image"])
+    #   end
+    # end
+  end
+end
