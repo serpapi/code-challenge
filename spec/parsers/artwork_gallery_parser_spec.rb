23b978b17c85953ab941cea65d87e4f7dc22b814
Oleksandr Manzyuk <admsev@gmail.com>
2025-05-03 05:01:02 +0200

    DRY: shared_example

--- spec/parsers/artwork_gallery_parser_spec.rb
+++ spec/parsers/artwork_gallery_parser_spec.rb
@@ -1,22 +1,5 @@
+# frozen_string_literal: true
+
 describe ArtworkGalleryParser do
-  let(:parser) { described_class.new }
-
-  describe '#parse' do
-    let(:file_path) { File.expand_path(File.join(File.dirname(__FILE__), '..', 'fixtures/pages/van-gogh-paintings.html')) }
-    let(:browser) { Ferrum::Browser.new(headless: true) }
-    let(:page) { browser.create_page }
-
-    before do
-      page.go_to("file://#{file_path}")
-    end
-
-    let(:expected_json) { File.read(File.join(File.dirname(__FILE__), '..', 'fixtures/expected/van-gogh-paintings.json')) }
-    let(:expected_json_data) { JSON.parse(expected_json, symbolize_names: true) }
-
-    subject { parser.parse(page) }
-
-    it 'returns expected JSON' do
-      expect(subject).to eq(expected_json_data)
-    end
-  end
+  it_behaves_like 'a gallery parser', 'van-gogh-paintings'
 end
---
+++ spec/support/shared_examples/gallery_parser.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a gallery parser' do |fixture_name|
+  let(:parser) { described_class.new }
+
+  describe '#parse' do
+    let(:file_path) do
+      File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'fixtures/pages', "#{fixture_name}.html"))
+    end
+    let(:browser) { Ferrum::Browser.new(headless: true) }
+    let(:page) { browser.create_page }
+
+    before do
+      page.go_to("file://#{file_path}")
+    end
+
+    after do
+      browser.quit
+    end
+
+    let(:expected_json) do
+      File.read(File.join(File.dirname(__FILE__), '..', '..', 'fixtures/expected', "#{fixture_name}.json"))
+    end
+    let(:expected_json_data) { JSON.parse(expected_json, symbolize_names: true) }
+
+    subject { parser.parse(page) }
+
+    it 'returns expected JSON' do
+      expect(subject).to eq(expected_json_data)
+    end
+  end
+end
