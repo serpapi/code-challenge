223d2a64f57c48f0b52a0a1266813c00c18b8476
Oleksandr Manzyuk <admsev@gmail.com>
2025-05-05 21:29:51 +0200

    Smplified InlineImagesGalleryParser

--- lib/parsers/inline_images_gallery_parser.rb
+++ lib/parsers/inline_images_gallery_parser.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# This class demonstrates a slightly different approach to code organization.
-# It introduces Element instance methods instead of utility methods.
-# Both approaches are valid, and encapsulation is key - parsers can determine their own implementation details.
-# This Element class approach can be made reusable for other/next parsers as well.
 class InlineImagesGalleryParser < BaseGalleryParser
   def self.elements_selector
     'div#iur div[data-docid]'
@@ -14,76 +10,69 @@ class InlineImagesGalleryParser < BaseGalleryParser
   end
 
   def element_data(element)
-    Element.new(element).as_json
+    image_js_data = image_data(element)
+
+    {
+      source: element_source(element),
+      thumbnail: image_js_data.dig(1, 2, 0),
+      original: image_js_data.dig(1, 3, 0),
+      title: element_title(element),
+      source_name: element_source_name(element)
+    }
   end
 
-  class Element
-    def initialize(dom_element)
-      @element = dom_element
-      @page = @element.page
-    end
+  private
 
-    def as_json
-      {
-        source: source,
-        thumbnail: thumbnail,
-        original: original,
-        title: title,
-        source_name: source_name
-      }
-    end
+  def element_source(element)
+    element.at_css('a')['href']
+  end
 
-    def source
-      @element.at_css('a')['href']
-    end
+  def element_title(element)
+    element.at_css('img').attribute('alt')
+  end
 
-    def thumbnail
-      image_data.dig(1, 2, 0)
-    end
+  def element_source_name(element)
+    element.at_css('a').xpath('../div/div/div')&.last&.text&.strip&.gsub(/\s+/, ' ')
+  end
 
-    def original
-      image_data.dig(1, 3, 0)
-    end
+  def element_window_javascript_data_id(element)
+    element.attribute('jsdata').split(';').last
+  end
 
-    def title
-      @element.at_css('img').attribute('alt')
-    end
+  def image_data(element)
+    images_data[element_window_javascript_data_id(element)]
+  end
 
-    def source_name
-      @element.at_css('a').xpath('../div/div/div')&.last&.text&.strip&.gsub(/\s+/, ' ')
-    end
+  def displayed_image_data(element)
+    displayed_images_data[element_window_javascript_data_id(element)]
+  end
 
-    private
+  def hidden_image_data(element)
+    hidden_images_data[element_window_javascript_data_id(element)]
+  end
 
-    def window_javascript_data_id
-      @window_javascript_data_id ||= @element.attribute('jsdata').split(';').last
-    end
+  def images_data
+    @images_data ||= displayed_images_data.merge(hidden_images_data)
+  end
 
-    def window_javascript_data
-      @window_javascript_data ||= @page.evaluate('W_jd')
-    end
+  def displayed_images_data
+    @displayed_images_data ||= @page.evaluate('W_jd')
+  end
+  
+  # WARNING: This code heavily relies on the page's JavaScript code.
+  # For hidden images, their data is not present in W_jd in the initial page state.
+  # I am scanning the page for key => data pairs and then evaluating the JavaScript data code for every key.
+  # This code is a bit of a hack, but it works for now.
+  # It is only a matter of time before Google changes their code.
+  # Automated E2E tests are recommended for early detection of changes.
+  # The SerpAPI team might know better ways to deal with hidden elements.
+  def hidden_images_data
+    @hidden_images_data ||= begin
+      # this scans the page for pairs key => data
+      pairs = @page.body.scan(/window\['W_jd'\]\[\'([0-9a-zA-Z]+)\'\] ?= ?([^\;]+\;)/)
 
-    def image_data
-      @image_data ||= displayed_image_data || hidden_image_data
-    end
-
-    def displayed_image_data
-      @displayed_image_data ||= window_javascript_data[window_javascript_data_id]
-    end
-
-    # ATTENTION: This relies heavily on the page's JavaScript code
-    # Hidden images' data is not automatically added to window.W_jd
-    # This method extracts data by parsing the JavaScript code
-    # Solution works but is fragile
-    # Regular automated e2e testing recommended to detect Google's JavaScript changes
-    # There might be a way to trigger the code execution for hidden images, but I'm limited by time.
-    def hidden_image_data
-      @hidden_image_data ||= begin
-        js = @page.body.scan(/(window\['W_jd'\]\['#{window_javascript_data_id}'\] ?= ?([^;]+;))/)
-        js_data = js.dig(0, 1)
-
-        @page.evaluate(js_data)
-      end
+      # this evaluates data javascript code for every key
+      Hash[pairs.map { |w_jd_id, js_value| [w_jd_id, @page.evaluate(js_value)] }]
     end
   end
 end
