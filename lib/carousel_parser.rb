5cc85a0d5d3aceb189bb59c29d339a9bb38b00a6
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2025-08-06 23:12:45 +0200

    parse images correctly

--- lib/carousel_parser.rb
+++ lib/carousel_parser.rb
@@ -21,7 +21,8 @@ class CarouselParser
     {
       name: name(item),
       extensions: extensions(item),
-      link: link(item)
+      link: link(item),
+      image: image(item)
     }
   end
 
@@ -30,11 +31,39 @@ class CarouselParser
   end
 
   def extensions(item)
-    item.at_css('div').children[1..].map {|extension| extension.text }
+    item.at_css('div').children[1..].map { |extension| extension.text }
   end
 
   def link(item)
     item['href']
+  end
+
+  def image(item)
+    image_elem = item.css('img')
+    image_id = image_elem.first['id']
+
+    if image_id
+      load_image_from_script(image_id)
+    else
+      load_image_from_data_src(image_elem)
+    end
+  end
+
+  def load_image_from_script(image_id)
+    script = script_tags.select { |scr| scr.text.include?(image_id)}
+    extract_image(script)
+  end
+
+  def extract_image(script)
+    script.first.text[/s='(.*?)';/, 1]
+  end
+
+  def load_image_from_data_src(image_elem)
+    image_elem.first['data-src']
+  end
+
+  def script_tags
+    @script_tags ||= html.css('script')
   end
 
   def get_carousel_division
