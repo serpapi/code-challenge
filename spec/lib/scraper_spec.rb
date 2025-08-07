fef7d184fa72cfb35cfa37d970ab97e5b5ee5ab3
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2025-08-07 21:00:54 +0200

    add specs for monet paintings

--- spec/lib/scraper_spec.rb
+++ spec/lib/scraper_spec.rb
@@ -6,10 +6,21 @@ require_relative '../../lib/scraper'
 RSpec.describe Scraper do
   subject { JSON.parse(Scraper.new(path).call) }
 
-  let(:path) { './files/van-gogh-paintings.html' }
-  let(:expected_json) { JSON.parse(File.read('./files/expected-array.json')) }
+  context 'van gogh paintings' do
+    let(:path) { './files/van-gogh-paintings.html' }
+    let(:expected_json) { JSON.parse(File.read('./files/expected-array.json')) }
 
-  it 'parses the HTML and returns the expected JSON' do
-    expect(subject).to eq(expected_json)
+    it 'parses the HTML and returns the expected JSON' do
+      expect(subject).to eq(expected_json)
+    end
+  end
+
+  context 'monet paintings' do
+    let(:path) { './files/monet-paintings.html' }
+    let(:expected_json) { JSON.parse(File.read('./files/monet-paintings-expected-array.json')) }
+
+    it 'parses the HTML and returns the expected JSON' do
+      expect(subject).to eq(expected_json)
+    end
   end
 end
