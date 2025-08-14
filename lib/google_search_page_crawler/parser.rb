850e8e153dc07c3caaa073d43dcfca132956f401
226351850+vaocode@users.noreply.github.com <226351850+vaocode@users.noreply.github.com>
2025-08-14 19:23:14 -0300

    Implements parser parse_artwork for titles

--- lib/google_search_page_crawler/parser.rb
+++ lib/google_search_page_crawler/parser.rb
@@ -2,15 +2,24 @@ require 'nokogiri'
 
 class GoogleSearchPageCrawler
   class Parser
+    attr_reader :doc
     def initialize(html)
-      @doc = Nokogiri::HTML(file_content)
+      @doc = Nokogiri::HTML(html)
     end
 
     def parse
       {
-        "Artworks": [
+        "artworks": parse_artworks
+      }
+    end
 
-        ]
+    def parse_artworks
+      doc.css('kc:/visual_art/visual_artist:works"] a').map(&:parse_artwork)
+    end
+
+    def parse_artwork(artwork_node)
+      {
+        "title": artwork_node.css("div > div").first.text
       }
     end
   end
