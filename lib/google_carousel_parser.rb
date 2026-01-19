012ab3132e0fe42e18180a3b45bff4a59029206f
TheSmartnik <misharinn@gmail.com>
2026-01-19 11:11:38 +0400

    Fix for other layouts

--- lib/google_carousel_parser.rb
+++ lib/google_carousel_parser.rb
@@ -1,40 +1,54 @@
 require 'nokogiri'
 
 class GoogleCarouselParser
-  attr_reader :html
+  attr_reader :html, :attr_id
+
+  ATTR_ID_REGEX = %r{kc:/\w*/\w*:\w*}
 
   def initialize(html)
+    @attr_id = html.match(ATTR_ID_REGEX).to_a.first
     @html = Nokogiri::HTML(html)
   end
 
   def call
-    html.css('div[data-attrid="kc:/visual_art/visual_artist:works"] a').map do |link|
-      parse_link(link)
-    end
+    carousel_block = html.at_css("div[data-attrid='#{attr_id}']")
+    heading = html.at_css('[role="tab"][aria-selected="true"]')&.text&.downcase || extract_heading(carousel_block)
+    data = carousel_block.css('a').map { |el| parse_element(el) }.compact
+
+    { heading => data }
   end
 
   private
 
-  def parse_link(link)
-    img_attrs = link.css('img').first.attributes
-    name = img_attrs['alt'].value
-    extensions = link.text.gsub(name, '')
+  def parse_element(el)
+    img_attrs = el.at_css('img')&.attributes
+    return unless img_attrs
+
+    name, *extensions = el.search('text()').map(&:text)
     img_src = img_attrs.key?('id') ? image_sources[img_attrs['id'].value] : img_attrs['data-src'].value
 
-    attrs = {
-      'link' => "https://www.google.com#{link.attributes['href'].value}",
-      'name' => name,
-      'image' => img_src
-    }
-    attrs.merge!('extensions' => [extensions]) unless extensions.empty?
+    attrs = { 'link' => construct_link(el), 'name' => name, 'image' => img_src }
+    attrs.merge!('extensions' => extensions) unless extensions.empty?
     attrs
+  end
+
+  def construct_link(el)
+    val = el.attributes['href'].value
+    val.start_with?('http') ? val : "https://www.google.com#{val}"
+  end
+
+  def extract_heading(carousel)
+    heading = carousel.parent.at_css("div[role='heading']")
+    return heading.search('text()').last.text.downcase if heading
+
+    extract_heading(carousel.parent)
   end
 
   def image_sources
     @image_sources ||= begin
       scripts = html.css('script').map(&:text).select { _1.include?('_setImagesSrc') }
       scripts.each_with_object({}) do |script, acc|
-        src, id = script.match(/.*(data:image.*)';.*\['(\w*)'\];/)&.captures
+        src, id = script.match(/.*(data:image.*)';.*\['([\w-]*)'\];/)&.captures
         acc[id] = replace_js_escape_sequences(src) if src && id
       end
     end
--- spec/lib/google_carousel_parser_spec.rb
+++ spec/lib/google_carousel_parser_spec.rb
@@ -7,11 +7,47 @@ RSpec.describe GoogleCarouselParser do
   subject(:results) { described_class.new(html).call }
 
   context 'when carousel is artworks' do
-    let(:html) { File.read('files/van-gogh-paintings.html') }
-    let(:json){ JSON.parse(File.read('files/expected-array.json')) }
+    let(:html) { File.read('files/van-gogh/van-gogh-paintings.html') }
+    let(:json){ JSON.parse(File.read('files/van-gogh/expected-array.json')) }
 
     it 'parses artworks correctly' do
-      expect(results).to eq json['artworks']
+      expect(results).to eq json
+    end
+  end
+
+  context 'when carousel is sport roaster' do
+    let(:html) { File.read('files/lakers-roaster/lakers-roaster.html') }
+    let(:json){ JSON.parse(File.read('files/lakers-roaster/expected-array.json')) }
+
+    it 'parses correctly' do
+      expect(results).to eq json
+    end
+  end
+
+  context 'when carousel is tv cast' do
+    let(:html) { File.read('files/stranger-things-cast/index.html') }
+    let(:json){ JSON.parse(File.read('files/stranger-things-cast/expected-array.json')) }
+
+    it 'parses correctly' do
+      expect(results).to eq (json)
+    end
+  end
+
+  context 'when carousel is a band' do
+    let(:html) { File.read('files/jackson5-members/index.html') }
+    let(:json){ JSON.parse(File.read('files/jackson5-members/expected-array.json')) }
+
+    it 'parses correctly' do
+      expect(results).to eq (json)
+    end
+  end
+
+  context 'when carousel is a band' do
+    let(:html) { File.read('files/kanye-albums/index.html') }
+    let(:json){ JSON.parse(File.read('files/kanye-albums/expected-array.json')) }
+
+    it 'parses correctly' do
+      expect(results).to eq (json)
     end
   end
 end
