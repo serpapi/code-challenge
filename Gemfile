2291caab4f4356070150ba91d084fca9c0f8fa1a
Jason Dinsmore <jason@dinjas.com>
2026-06-23 11:08:34 -0700

    Add rubocop + minimal config; address lint

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ gem "nokogiri", "~> 1.19"
 group :test do
   gem "rspec", "~> 3.13"
 end
+
+group :development, :test do
+  gem "rubocop", "~> 1.88"
+end
--- lib/carousel_extractor.rb
+++ lib/carousel_extractor.rb
@@ -2,6 +2,9 @@
 
 require "nokogiri"
 
+# Extracts a Google Knowledge Graph entity carousel (e.g.: paintings, albums,
+# buildings, movie cast, etc.) into a uniform array of objects:
+# `{ name:, extensions:, link:, image: }`.
 class CarouselExtractor
   def self.call(_html) = []
 end
